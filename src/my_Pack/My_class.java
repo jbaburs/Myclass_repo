6ef3f26a80f97cd13ca7aea6f0c8337c06590b48
Babu Jana <jbaburs@gmail.com>
2024-04-19 16:04:42 +0530

    initial commit

---
+++ src/my_Pack/My_class.java
@@ -0,0 +1,19 @@
+package my_Pack;
+
+public class My_class {
+
+	public static void main(String[] args) {
+		
+        String s1 = "God";
+		String s2 = "is";
+		String s3 = "Great";
+		
+		System.out.println(s1+10+15+s2+s3);
+		System.out.println(s1+10*15+s2+s3);
+		System.out.println(s1+s2+s3+10*5);
+		System.out.println(s1+(10+15)+s2+s3);
+	
+
+	}
+
+}
